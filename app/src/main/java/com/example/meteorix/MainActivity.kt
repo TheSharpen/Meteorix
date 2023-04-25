6b4d80d6826662fbc39a6405ad32411e97b1bf57
TheSharpen <sh4rp3n@gmail.com>
2023-04-25 22:55:16 +0200

    MeteoriteList showing correctly + UI adjustment

--- app/src/main/java/com/example/meteorix/MainActivity.kt
+++ app/src/main/java/com/example/meteorix/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.meteorix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.meteorix.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var meteoriteAdapter: MeteoriteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                binding.progressBar.isVisible = true
+                val response = try {
+                    RetrofitInstance.api.getMeteorites()
+                } catch (e: IOException) {
+                    Log.d("TAG","IOException, please check your internet connection")
+                    binding.progressBar.isVisible = false
+                    return@repeatOnLifecycle
+                } catch (e: HttpException) {
+                    Log.d("TAG","Http Exception, please check your internet connection")
+                    binding.progressBar.isVisible = false
+                    return@repeatOnLifecycle
+                }
+                if (response.isSuccessful && response.body() != null ) {
+                    meteoriteAdapter.meteorites = response.body()!!
+                    Log.d("XLOG","${meteoriteAdapter.meteorites}")
+                } else {
+                    Log.e("TAG", "Response failed")
+                }
+                binding.progressBar.isVisible = false
+            }
+        }
     }
+
+
+
+    private fun setupRecyclerView() = binding.rvMeteors.apply {
+        meteoriteAdapter = MeteoriteAdapter()
+        adapter = meteoriteAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+
+
+
+
 }
